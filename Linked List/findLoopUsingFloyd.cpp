349ac2ee497e5bf50b69fdaa56d99a3a731f76c2
Arghya <arghyadas242004@gmail.com>
2022-05-08 23:35:36 +0530

    Find and remove Loops in LL

---
+++ Linked List/findLoopUsingFloyd.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* next ;
+
+    Node(int d)
+    {
+        this->data = d;
+        this->next = NULL ;
+    }
+};
+
+void print(Node* head)
+{
+    cout<<"LL : " ;
+    Node* temp = head ;
+    while (temp != NULL)
+    {
+        cout<<temp->data<<" " ;
+        temp=temp->next ;
+    }
+}
+
+bool check(Node* head)
+{
+    if (head==NULL)
+        return true ;
+    Node* fast = head ;
+    Node* slow = head ;
+    while (fast!=NULL)
+    {
+        fast = fast->next ;
+        if (fast!=NULL)
+        {
+            fast = fast->next ;
+        }
+        
+        slow = slow->next ;
+
+
+        if (slow==fast)
+            return true ;
+    }
+    
+    return false ;
+}
+
+
+int main(){
+    Node* n1 = new Node(10) ;
+    Node* head = n1 ;
+    Node* n2 = new Node(5) ;
+    n1->next = n2 ;
+    Node* n3 = new Node(9) ;
+    n2->next = n3 ;
+    Node* n4 = new Node(2) ;
+    n3->next = n4 ;
+    Node* n5 = new Node(8) ;
+    n4->next = n5 ;
+    Node* n6 = new Node(3) ;
+    n5->next = n6 ;
+    n6->next = n3 ;
+
+    /*
+    10 -> 5 -> 9 -> 2 -> 8 -> 3
+               ⬆⬅ ⬅ ⬅ ⬅ ⬅⬇
+    */
+    if (check(head))
+    {
+        cout<<"LOOP Exists" ;
+    }
+    else
+    {
+        cout<<"LOOP doesn't exist" ;
+    }
+
+    
+
+    return 0 ;
+}
---
+++ Linked List/removeLoopUsingFloyd.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* next ;
+
+    Node(int d)
+    {
+        this->data = d;
+        this->next = NULL ;
+    }
+};
+
+void print(Node* head)
+{
+    cout<<"LL : " ;
+    Node* temp = head ;
+    while (temp != NULL)
+    {
+        cout<<temp->data<<" " ;
+        temp=temp->next ;
+    }
+}
+
+Node* check(Node* head)
+{
+    if (head==NULL)
+        return head ;
+    Node* fast = head ;
+    Node* slow = head ;
+    while (fast!=NULL)
+    {
+        fast = fast->next ;
+        if (fast!=NULL)
+        {
+            fast = fast->next ;
+        }
+        
+        slow = slow->next ;
+        
+        if (slow==fast)
+            break ;
+    }
+    // now put slow to the head position again
+    // but this time the speed of slow and fast will be equal = unity
+    slow = head ;
+    while (slow->next!=fast->next)
+    {
+        slow = slow->next ;
+        fast = fast->next ;
+    }
+
+    fast->next = NULL ;
+
+    return fast;
+}
+
+
+int main(){
+    Node* n1 = new Node(10) ;
+    Node* head = n1 ;
+    Node* n2 = new Node(5) ;
+    n1->next = n2 ;
+    Node* n3 = new Node(9) ;
+    n2->next = n3 ;
+    Node* n4 = new Node(2) ;
+    n3->next = n4 ;
+    Node* n5 = new Node(8) ;
+    n4->next = n5 ;
+    Node* n6 = new Node(3) ;
+    n5->next = n6 ;
+    n6->next = n3 ;
+
+    /*
+    10 -> 5 -> 9 -> 2 -> 8 -> 3
+               ⬆⬅ ⬅ ⬅ ⬅ ⬅⬇
+    */
+    
+    Node* ans = check(head) ;
+
+    cout<<"Loop is terminated at : "<<ans->data<<endl ;
+    return 0 ;
+}
