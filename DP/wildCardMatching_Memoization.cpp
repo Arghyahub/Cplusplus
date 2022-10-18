d6939df0cdfa59053a795a38bfb5078107cfcd8e
Arghya <arghyadas242004@gmail.com>
2022-10-18 22:06:15 +0530

    Add Memoization done

---
+++ DP/wildCardMatching_Memoization.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    bool solve(string &s, string &p, int i, int j, vector<vector<int>> &dp)
+    {
+        if (i==0 && j==0) return true;
+        if (i>=0 && j==0) return false;
+        if (i==0 && j>0 ){
+            for (int k=1; k<=j ; k++)
+                if (p[k-1]!='*')
+                    return false;
+            return true;
+        }
+        
+        if (dp[i][j]!=-1) return dp[i][j] ;
+        
+        if (s[i-1]==p[j-1] || p[j-1]=='?')
+            return dp[i][j] = solve(s,p,i-1,j-1,dp) ;
+        else if (p[j-1]=='*')
+            return dp[i][j] = ( solve(s,p,i-1,j,dp) || solve(s,p,i,j-1,dp)  ) ;
+        //else
+        return dp[i][j] = false;
+    }
+public:
+    bool isMatch(string &s, string &p) {
+        int n = s.length() , m = p.length() ;
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1)) ;
+        return solve(s,p,n,m,dp) ;
+    }
+};
