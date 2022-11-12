83437abda63b78ca5d2d7f2fdbe7c1aae256be64
Arghya <arghyadas242004@gmail.com>
2022-11-13 00:02:24 +0530

    Fix: Updated with correct code

--- Practice/addingPrimeFactorsInSieveOfEratosthenes.cpp
+++ Practice/addingPrimeFactorsInSieveOfEratosthenes.cpp
@@ -1,21 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
+    // Q Find the sum of all the numbers between the range l and r
+    // Here each number is represented by the sum of its distinct prime factors. 
+    // each number like 6 has prime factors 2,3 => 2+3=5
 class Solution {
   public:
     int sumOfAll(int l, int r){
-        vector<int> primesum(r+1,0);
-        primesum[1]=1;
-        for(int i=2;i<=r;i++){
-            if(primesum[i]==0){
-                primesum[i]=i;
-                for(int j=i+i;j<=r;j+=i)
-                    primesum[j] = primesum[j] + i;
+        vector<int> sieve(r+1,0) ;
+        for (int i=2; i<=r; i++){
+            if (sieve[i]==0){
+                sieve[i] = i;
+                
+                for (int k=2; i*k<=r; k++){
+                    sieve[i*k] += i;
+                }
+                
             }
         }
-        int sum=0;
-        for(int i=l;i<=r;i++){
-            sum+=primesum[i];
+        
+        int ans=0;
+        for (int i=l; i<=r; i++){
+            ans+=sieve[i] ;
         }
-        return sum;
+        return ans;
     }
 };
