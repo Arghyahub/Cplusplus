9de0a4c8e5837cad9ab6aa996df6673a7856bddf
Arghya <arghyadas242004@gmail.com>
2022-11-14 14:33:13 +0530

    Doc : Question added

--- bitManipulation/BaseEquivalence.cpp
+++ bitManipulation/BaseEquivalence.cpp
@@ -1,5 +1,9 @@
 class Solution {
   public:
+    /**
+     *  Q. GIVEN NUMBER N AND NUMBER OF DIGITS M, USE ANY BASE FROM 2 TO 32, AND CHECK IF YOU CAN REPRESENT
+     *  THE NUMBER IN M BITS
+     */
     string baseEquiv(int n, int m){
         for (int i=2; i<=32; i++){
             if ( pow(i,m-1) > n ) continue;	// THE FIRST POWER CANNOT BE ZERO, SO WE HAVE TO CHECK IT
