beaa65d488cc8bb3d761bb36ba150a2b50b5ddb6
Arghya <arghyadas242004@gmail.com>
2022-11-15 15:14:57 +0530

    Doc Added the output

--- recursion/Permutation.cpp
+++ recursion/Permutation.cpp
@@ -2,6 +2,8 @@
 #include<vector>
 using namespace std;
 
+// [1,2,3] , [1,3,2] , [2,1,3] , [2,3,1] , [3,2,1] , [3,1,2] , 
+
 void solve(vector<int> nums,vector<vector<int>> &ans, int index)
 {
     if (index>=nums.size())
