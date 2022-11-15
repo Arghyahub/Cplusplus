78624fca28548c307d4a6354bfb5d60843a67f96
Arghya <arghyadas242004@gmail.com>
2022-11-15 15:33:42 +0530

    Fix : Fixed the documentation

--- recursion/PhoneNumber.cpp
+++ recursion/PhoneNumber.cpp
@@ -5,26 +5,26 @@
 using namespace std;
 
 void solve(string digits,vector<string> mapped,vector<string> &ans,string output,int index) 
+{
+    if (index>=digits.length()) // we see when given an input of 2 digits, ans is also of 2 digits
     {
-        if (index>=digits.length()) // we see when given an input of 2 digits, ans is also of 2 digits
-        {
-            ans.push_back(output) ;
-            return ;
-        }
-        
-        // The digits are given in string , conver them to int and find mappings
-        int n = digits[index] - '0' ;
-        string str = mapped[n] ;
-        
-        for (int i=0; i<str.length(); i++)
-        {
-            output.push_back(str[i]) ;
-            solve(digits,mapped,ans,output,index+1) ;
-            
-            // remove the previous entered character
-            output.pop_back() ;
-        }
+        ans.push_back(output) ;
+        return ;
     }
+    
+    // The digits are given in string , convert them to int and find mappings
+    int n = digits[index] - '0' ;
+    string str = mapped[n] ;
+    
+    for (int i=0; i<str.length(); i++)
+    {
+        output.push_back(str[i]) ;
+        solve(digits,mapped,ans,output,index+1) ;
+        
+        // remove the previous entered character
+        output.pop_back() ;
+    }
+}
 
 int main()
 {
@@ -35,9 +35,8 @@ int main()
         cout<<"" ;
         // return ans ; 
     vector<string> mapped = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"} ;
-    string output ;
-    int index=0 ;
-    solve(digits,mapped,ans,output,index) ;
+    
+    solve(digits,mapped,ans,"",0) ;
     // return ans ; 
 
     for (int i=0; i<ans.size(); i++) cout<<ans[i]<<" " ;
