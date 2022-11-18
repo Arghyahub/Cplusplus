ca8ae5f99744fa8466b962784b42ca0381b8260a
Arghya <arghyadas242004@gmail.com>
2022-11-18 16:56:33 +0530

    Doc comments added

--- recursion/course/kthSymbolInGrammar.cpp
+++ recursion/course/kthSymbolInGrammar.cpp
@@ -1,7 +1,9 @@
+// https://leetcode.com/problems/k-th-symbol-in-grammar/
 class Solution {
     int solve(int n , int k){
         if (n==1) return 0 ;
         
+        // IF K IS ODD, THE PARENT WILL BE (K+1)/2 , IF EVEN, PARENT WILL BE K/2
         bool parent = (k&1)? solve(n-1,(k+1)/2) : solve(n-1,k/2) ;
         
         if (parent){ // 1
