f871662bcb928c25b6190182bdd3e2a1e0e98f0d
Arghya <arghyadas242004@gmail.com>
2022-11-21 00:50:36 +0530

    Add Better Solution to regulat expression matching problem

--- DP/RegularExpressionMatchingSpaceOptimised.cpp
+++ DP/RegularExpressionMatchingSpaceOptimised.cpp
@@ -8,37 +8,34 @@ public:
 
         up[0] = true; // base case 1
 
-        for (int i=0; i<=n; i++){
-            for (int j=1; j<=m; j++){
-                if (i==0){
-                    bool flag = true;
-                    for (int k=j-1; k>=0; k--){
-                        if (p[k]=='*')
-                            k--;
-                        else{
-                            up[j] = false;
-                            flag=false;
-                        }
-                    }
-                    if (flag)
-                        up[j] = true;
-                }
-                else 
-                {    if (s[i-1]==p[j-1] || p[j-1]=='.')
-                        down[j] = up[j-1] ;
-                    // pres empty || pres multiple
-                    else if (p[j-1]=='*'){
-                        bool check=false;
-                        if (s[i-1]==p[j-2] || p[j-2]=='.')
-                            check = up[j] ; 
-                        down[j] = ( down[j-2] || check ) ;
-                    }
-                    else
-                        down[j] = false;
+        for (int j=1; j<=m; j++){       // base case 2
+            bool flag = true;
+            for (int k=j-1; k>=0; k--){
+                if (p[k]=='*')
+                    k--;
+                else{
+                    up[j] = false;
+                    flag=false;
                 }
             }
-            if (i!=0)
-                up=down;
+            if (flag)
+                up[j] = true;
+        }
+
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=m; j++){
+                if (s[i-1]==p[j-1] || p[j-1]=='.')
+                    down[j] = up[j-1] ;
+                else if (p[j-1]=='*'){
+                    bool check=false;
+                    if (s[i-1]==p[j-2] || p[j-2]=='.')
+                        check = up[j] ; 
+                    down[j] = ( down[j-2] || check ) ;
+                }
+                else
+                    down[j] = false;
+            }
+            up=down;
         }
 
         return up[m] ;
